7ab45755a018c3921ea6e79191453be1fb09c8f7
Alain Lehmann <alain.lehmann@gmail.com>
2021-10-25 22:02:02 +0200

    add minimal home manager configuration

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = input@{ self, nixpkgs, home-manager }: {
+    homeManagerConfigurations = {
+      base = home-manager.lib.homeManagerConfiguration {
+        configuration = {};
+        system = "x86_64-darwin";
+        homeDirectory = "/Users/ale";
+        username = "ale";
+      };
+    };
   };
 }
