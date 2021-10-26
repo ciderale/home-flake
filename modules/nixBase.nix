50958fc377ff9b89545b4572c2d655d1c60e6fb6
Alain Lehmann <alain.lehmann@gmail.com>
2021-10-26 22:09:33 +0200

    improve home manager convenience aliases

--- modules/nixBase.nix
+++ modules/nixBase.nix
@@ -8,8 +8,10 @@ in
   programs.home-manager.enable = true;
   home.packages = [ nixFlake ];
   programs.zsh.shellAliases = {
+    hmSwitchExistingResult = "./result/activate && source ~/.zshrc";
     hmPull = "nixFlake build . --update-input home-flake";
+    hmPullSwitch = "hmPull && hmSwitchExistingResult";
     hmLocalBuild = "nixFlake build . --override-input home-flake ./home-flake";
-    hmLocalSwitch = "hmLocalBuild && ./result/activate && source ~/.zshrc";
+    hmLocalSwitch = "hmLocalBuild && hmSwitchExistingResult";
   };
 }
